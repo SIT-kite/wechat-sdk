dd66ff3ebf8a6c21d28be92e91f887f23e90f74b
sunnysab <i@sunnysab.cn>
2021-08-05 22:57:24 +0800

    添加 example: code2session, 支持使用 code 转换得到 session

---
+++ examples/code2session.rs
@@ -0,0 +1,21 @@
+use wechat_sdk::WxClientError;
+use wechat_sdk::wechat::Login;
+
+#[tokio::main]
+async fn main() {
+    let client = wechat_sdk::WeChatClientBuilder::new()
+        .appid("no_such_appid")
+        .secret("secret")
+        .build();
+
+    let result = client.code2session("no_such_code").await;
+    match result {
+        Ok(session) => println!("Get session successfully, openid = {}", session.openid),
+        Err(e) => {
+            match e {
+                WxClientError::Api(api_err) => println!("Failed to get session, error code is {}, and reason: {:?}", api_err.code, api_err.msg),
+                WxClientError::Inner(other_err) => println!("Client error: {:?}", other_err.to_string()),
+            }
+        }
+    }
+}
